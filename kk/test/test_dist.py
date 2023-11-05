397c56372890a2b0c6c6eb53637f6b10a0e686ca
kkind <84040776@qq.com>
2023-11-05 12:02:29 +0800

    Merge remote-tracking branch 'origin/master'

--- kk/test/test_dist.py
+++ kk/test/test_dist.py
@@ -16,6 +16,20 @@ class TestModel(nn.Module):
         return self.net(x)
 
 
+class user_dataset(nn.utils.data.Dataset):
+    def __init__(self):
+        super(user_dataset, self).__init__()
+        self.x = torch.arange(10 * 5, dtype=torch.float32).view(10, 5)
+        self.y = torch.ones(10, dtype=torch.float32)
+        self.data = list(zip(self.x, self.y))
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return len(data)
+
+
 def test():
     x = torch.arange(10 * 5, dtype=torch.float32).view(10, 5)
     y = torch.ones(10, dtype=torch.float32)
