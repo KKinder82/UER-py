0ad5c4be1c8a14ddaf87de8706755b932ef8c3d7
kkind <84040776@qq.com>
2023-11-13 01:43:43 +0800

    Merge remote-tracking branch 'origin/master'

--- kk/apps/kk_app.py
+++ kk/apps/kk_app.py
@@ -625,7 +625,13 @@ class KkTrain(KkApp):
                 val_loss, val_perc = self._val(iepoch)
                 if val_loss < self.config.stop_train_loss:
                     print("  >> KkTrain.train << Rank {} : 当前预测精度已满足系统设计要求，训练结束。".format(self.config.rank))
+                    # 在训练结束时进行同步
+                    dist.barrier()
                     exit(0)
+                    # 在训练结束时进行全局归约
+                    # for param in model.parameters():
+                    #     dist.all_reduce(param.data, op=dist.reduce_op.SUM)
+                    #     param.data /= dist.get_world_size()
         finally:
             self._device_uninit()
 
